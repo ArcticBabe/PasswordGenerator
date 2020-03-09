4884794916566e242ff7b1efdb29d6805c7bff9b
ArcticBabe <hector_espinoza02@hotmail.com>
2020-03-09 01:49:34 -0700

    hi

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from random import choice
 def Gen(mode, lenght):
     count = 0
     password = ''
-    if mode == 1:
+    if mode == 1: # Mode 1 uses punctuation
         chars = uppercase + lowercase + digits + punctuation
         while count != lenght:
             password += password.join(choice(chars))
             count += 1
         return password
-    elif mode == 2:
+    elif mode == 2: # Mode 2 does not use punctuation
         chars = uppercase + lowercase + digits
         while count != lenght:
             password += password.join(choice(chars))
