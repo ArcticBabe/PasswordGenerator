396c424692d2f4105eb881a5045ddaa38ea99be6
ArcticBabe <hector_espinoza02@hotmail.com>
2020-03-23 00:15:51 -0700

    Updated to match PEP 8

--- main.py
+++ main.py
@@ -5,24 +5,34 @@ from string import digits
 from string import punctuation
 from random import choice
 
-# Start 
-
+# Start
 def Gen(mode, lenght):
+    
+    ''' Creates a password using the given parameters,
+    such as the mode of the gen and the lenght of the
+    password.'''
+
     count = 0
     password = ''
-    if mode == 1: # Mode 1 uses punctuation
+    if mode == 1:  # Mode 1 uses punctuation
         chars = uppercase + lowercase + digits + punctuation
         while count != lenght:
             password += password.join(choice(chars))
             count += 1
-    elif mode == 2: # Mode 2 does not use punctuation
+    elif mode == 2:  # Mode 2 does not use punctuation
         chars = uppercase + lowercase + digits
         while count != lenght:
             password += password.join(choice(chars))
             count += 1
-    else:
-        print('That mode dont exist')
+
     return password
 
+
 if __name__ == "__main__":
-    print(f'Mode 1 (With Punctuation): \n{Gen(1, 15)} \n\nMode 2: (Without Punctiation)\n{Gen(2,15)}')
+    print(f'''
+    Mode 1 (With Punctuation):
+    {Gen(1, 15)}
+    Mode 2: (Without Punctiation)
+    {Gen(2,15)}
+''')
+
